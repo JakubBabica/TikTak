f4e325a2dd2719432021d6c9e4f6d2f2ae0c55d2
Jakub babica <315190@via.dk>
2023-03-19 18:59:44 +0100

    final touches

--- object_oriented.js
+++ object_oriented.js
@@ -1,48 +1,39 @@
 
-let s1 = null
-let s2 = null
-let s3 = null
-let s4 = null
-let s5 = null
-let s6 = null
-let s7 = null
-let s8 = null
-let s9 = null
-
-
 class circle{
-    constructor(){}
+   
+    flag = 0;
+    squares = ["a","a","a","a","a","a","a","a","a"]
 
-    static flag = 0;
-      addMark(squareId) 
+
+   addMark(id) 
     {
-     const square = document.getElementById(squareId);
-     if (square.disabled) {
-       return; // Square is already disabled, do nothing
-     }
-     if (circle.flag == 0) {
-       square.innerHTML = "X";
-       square.disabled = true;
-       circle.flag = 1;
-     } else {
-       square.innerHTML = "O";
-       square.disabled = true;
-       circle.flag = 0;
-     }
-   }
-   winnerCheck(){
-        s1 = document.getElementById("square1");
-        s2 = document.getElementById("square2");
-        s3 = document.getElementById("square3");
-        s4 = document.getElementById("square4");
-        s5 = document.getElementById("square5");
-        s6 = document.getElementById("square6");
-        s7 = document.getElementById("square7");
-        s8 = document.getElementById("square8");
-        s9 = document.getElementById("square9");
-        
-        
-        if ((s1.innerHTML=="X"&& s2.innerHTML=="X"&& s3.innerHTML=="X") 
+      const square =this.getSquares(id);
+      if (square.disabled) {
+        return; // Square is already disabled, do nothing
+      }
+      if (circle.flag == 0) {
+        square.innerHTML = "X";
+        square.disabled = true;
+        circle.flag = 1;
+      } else {
+        square.innerHTML = "O";
+        square.disabled = true;
+        circle.flag = 0;
+      }
+    }
+    winnerCheck()
+      {
+        s1 = this.squares[0];
+        s2 = this.squares[1];
+        s3 = this.squares[2];
+        s4 = this.squares[3];
+        s5 = this.squares[4];
+        s6 = this.squares[5];
+        s7 = this.squares[6];
+        s8 = this.squares[7];
+        s9 = this.squares[8];
+    
+        if((s1.innerHTML=="X"&& s2.innerHTML=="X"&& s3.innerHTML=="X") 
         || (s4.innerHTML=="X"&& s5.innerHTML=="X"&& s6.innerHTML=="X")
         || (s7.innerHTML=="X"&& s8.innerHTML=="X"&& s9.innerHTML=="X")
         || (s1.innerHTML=="X"&& s4.innerHTML=="X"&& s7.innerHTML=="X")
@@ -62,7 +53,29 @@ class circle{
         || (s3.innerHTML=="O"&& s5.innerHTML=="O"&& s7.innerHTML=="O")
         )
         {
-        alert("you won")
+            alert("you won")
         }
+      }
+
+    restart(){
+     for(let i = 0; i < 9; i++){
+      this.squares[i].innerHTML ="";
+      this.squares[i].disabled = false;
+     }
     }
- }
+
+    getSquares(id){
+      this.squares[id] = document.getElementById(id);
+      return this.squares[id]
+    }
+}
+let s1 = null
+let s2 = null
+let s3 = null
+let s4 = null
+let s5 = null
+let s6 = null
+let s7 = null
+let s8 = null
+let s9 = null
+var circle1 = new circle();
